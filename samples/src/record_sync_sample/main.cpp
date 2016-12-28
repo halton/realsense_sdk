02481617664c38bf21f6d0f50cbf0acf30b82b7e
Halton Huo <halton.huo@intel.com>
2016-12-28 17:11:14 +0800

    Adpat OR sample config.
    strem   | resolution | format | frame_rate
    ------------------------------------------
    color   | 640x480    | rgb8   | 30
    depth   | 320x240    | z16    | 30
    fisheye(disabled)

--- samples/src/record_sync_sample/main.cpp
+++ samples/src/record_sync_sample/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) try
     rs::device* device = context.get_device(0);
 
     //enable required streams
-    device->enable_stream(rs::stream::color, 320, 240, rs::format::rgb8, 30);
+    device->enable_stream(rs::stream::color, 640, 480, rs::format::rgb8, 30);
     device->enable_stream(rs::stream::depth, 320, 240, rs::format::z16, 30);
 
     vector<rs::stream> streams = { rs::stream::color, rs::stream::depth };
